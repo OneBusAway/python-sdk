8133b61a7776964d965c14276dba02d6639149e2
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-07-05 14:01:19 +0300

    refactor(client): update auth_headers method to return an empty dictionary

--- src/onebusaway/_client.py
+++ src/onebusaway/_client.py
@@ -130,8 +130,9 @@ class OnebusawaySDK(SyncAPIClient):
         return Querystring(array_format="repeat")
 
     @property
-    def auth_headers(self) -> httpx.Auth:
-        raise NotImplementedError("This auth method has not been implemented yet.")
+    @override
+    def auth_headers(self) -> dict[str, str]:
+        return {}
 
     @property
     @override
@@ -321,8 +322,9 @@ class AsyncOnebusawaySDK(AsyncAPIClient):
         return Querystring(array_format="repeat")
 
     @property
-    def auth_headers(self) -> httpx.Auth:
-        raise NotImplementedError("This auth method has not been implemented yet.")
+    @override
+    def auth_headers(self) -> dict[str, str]:
+        return {}
 
     @property
     @override
