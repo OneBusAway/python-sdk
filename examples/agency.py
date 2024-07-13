653bb1a1d1108a1acd1a37b0fa7e6475cfede10a
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-07-13 23:11:51 +0300

    refactor: Update main_sync function signature to include return type annotation

--- examples/agency.py
+++ examples/agency.py
@@ -1,7 +1,6 @@
 import onebusaway
 
-
-def main_sync():
+def main_sync() -> None:
     client = onebusaway.OnebusawaySDK(api_key="TEST")
     agency = client.agency.retrieve("1")
 
@@ -10,25 +9,6 @@ def main_sync():
     else:
         print("Agency data or entry is None.")
 
-
 if __name__ == "__main__":
     print("Running synchronous main function:")
     main_sync()
-
-# import asyncio
-# import sys
-
-# async def main_async():
-#     client = onebusaway.AsyncOnebusawaySDK(api_key="TEST")
-#     agency = await client.agency.retrieve('1')
-
-#     if agency.data and agency.data.entry:
-#         print(agency.data.entry)
-#     else:
-#         print("Agency data or entry is None.")
-
-# if __name__ == "__main__":
-#     if sys.platform.startswith('win'):
-#         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-#     print("Running asynchronous main function:")
-#     asyncio.run(main_async())
